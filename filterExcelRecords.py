9968982fbdcf819289677945aee0fea44f538719
abykov <albykov@gmail.com>
2016-05-10 14:52:47 -0600

    comments

--- filterExcelRecords.py
+++ filterExcelRecords.py
@@ -1,25 +1,44 @@
+#author: albykov@gmail.com
+#non standard includes: openpyxl
+#usage: scans excel spreadsheet and fills the gaps using previous rows
+
+#load library
 from openpyxl import load_workbook
+
+#load workbook to memory
 wb = load_workbook(r'D:\abykov\BigProjectFiles\151003 Lethbridge Ecological Inventory\titles_160411\RVMP titles222.xlsx')
-print wb.get_sheet_names()
+#print wb.get_sheet_names()
+
+#get active worksheet
 ws = wb.active
+
+#go through all rows
 for row in ws.iter_rows():
     takePrevVal = 0
+    #go through all cell of current row
     for cell in row:
+        #if this is a first column
         if cell.column == 'A':
+            #if this column is non None
             if cell.value is not None:
+                #save it to variable curr_row
                 curr_row = row
+                #if this cell is None
             elif cell.value is None:
+                #switch to fix it
                 takePrevVal = 1
                 print '------------None:' + str(cell.row)
+                #if fix switcher is on
         if takePrevVal:
+            #if current cell value is None
             if cell.value is None:
                 print 'Col: ' + str(cell.col_idx)
                 print 'Val+: ' + str(curr_row[cell.col_idx-1].value)
+                #if saved prev row cell value is non None
                 if curr_row[cell.col_idx-1].value is not None:
                     cell.value = curr_row[cell.col_idx-1].value
             else:
                 print 'Col: ' + str(cell.col_idx)
                 print 'Val: ' + str(cell.value)
-            #print curr_row[cell.col_idx-1].value
-
+#save workbook
 wb.save(r'D:\abykov\BigProjectFiles\151003 Lethbridge Ecological Inventory\titles_160411\RVMP titles222b.xlsx')
--- test.py
+++ test.py
@@ -1,5 +1,16 @@
-import arcpy
-print 'arcpy imported'
-xaskfhakjsf = 123123
+import openpyxl
 
-xa
+f_xlsx = r'C:\abykov\shared\projects\15_PythonTest\data\RVMPtitles.xlsx'
+wb = openpyxl.load_workbook(f_xlsx, use_iterators=True)
+wbfs = wb.get_sheet_names()[0]
+wsh = wb.get_sheet_by_name(wbfs)
+
+i = 1
+for row in wsh.iter_rows():
+    #print row
+    print i
+    for cell in row:
+        print cell.value
+    i = i + 1
+
+#from openpyxl import Workbook
