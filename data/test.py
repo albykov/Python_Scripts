1c9e03eb3bc00a85a1d2147271b62521813dae22
abykov <alexander.bykov@gmail.com>
2016-05-12 10:54:47 -0600

    update

--- data/test.py
+++
@@ -1,5 +0,0 @@
-__author__ = 'abykov'
-def getShortTitleNameNoSpaces(tn):
-    return tn.split('+')[0].replace(' ', '') + tn.split('+')[1].zfill(3)
-
-print getShortTitleNameNoSpaces('941 299 535 +3')
