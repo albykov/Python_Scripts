a267237ccd10d0375dedf20a8bb784fb67cac657
abykov <alexander.bykov@gmail.com>
2016-05-12 10:39:38 -0600

    update

--- myhelpers.py
+++ myhelpers.py
@@ -1,16 +1,12 @@
 __author__ = 'abykov'
 
 def getCurrentDateforFileName():
-    #other format %Y%m%d-%H%M%S
     import time
     return (time.strftime("%y%m%d"))
 
 def getCurrentDateTimeforFileName():
-    #other format %Y%m%d-%H%M%S
     import time
     return (time.strftime("%y%m%d%H%M"))
-
-#print getCurrentDateforFileName()
 
 def getNewFilePathWithDateNoSpaces(ffn):
     import os
@@ -21,5 +17,3 @@ def getNewFilePathWithDateNoSpaces(ffn):
     fDir = os.path.dirname(ffn)
 
     return fDir + '\\' +fn_noExt_NoSpaces + '_' +getCurrentDateTimeforFileName() + fExt
-
-print getNewFilePathWithDateNoSpaces(r'D:\abykov\BigProjectFiles\151003 Lethbridge Ecological Inventory\titles_160411\RVMP titles222.xlsx')
