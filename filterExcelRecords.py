4ebf1897f4aee6f0cb34b13b227893be6379629f
abykov <alexander.bykov@gmail.com>
2016-05-12 13:46:02 -0600

    update

--- filterExcelRecords.py
+++ filterExcelRecords.py
@@ -22,34 +22,39 @@ wb = load_workbook(wbPath)
 ws = wb.active
 
 #go through all rows
+isFirstRow = True
 for row in ws.iter_rows():
-    takePrevVal = 0
-    #go through all cell of current row
-    for cell in row:
-        #if this is a first column
-        if cell.column == 'A':
-            #if this column is non None
-            if cell.value is not None:
-                #save it to variable curr_row
-                curr_row = row
-                cell.value = getShortTitleNameNoSpaces(cell.value)
-                #if this cell is None
-            elif cell.value is None:
-                #switch to fix it
-                takePrevVal = 1
-                print '------------None:' + str(cell.row)
-                #if fix switcher is on
-        if takePrevVal:
-            #if current cell value is None
-            if cell.value is None:
-                print 'Col: ' + str(cell.col_idx)
-                print 'Val+: ' + str(curr_row[cell.col_idx-1].value)
-                #if saved prev row cell value is non None
-                if curr_row[cell.col_idx-1].value is not None:
-                    cell.value = curr_row[cell.col_idx-1].value
-            else:
-                print 'Col: ' + str(cell.col_idx)
-                print 'Val: ' + str(cell.value)
+    if not isFirstRow:
+        takePrevVal = 0
+        #go through all cell of current row
+        for cell in row:
+            #if this is a first column
+            if cell.column == 'A':
+                #if this column is non None
+                if cell.value is not None:
+                    #save it to variable curr_row
+                    curr_row = row
+                    print cell.row
+                    cell.value = getShortTitleNameNoSpaces(cell.value)
+                    #if this cell is None
+                elif cell.value is None:
+                    #switch to fix it
+                    takePrevVal = 1
+                    print '------------None:' + str(cell.row)
+                    #if fix switcher is on
+            if takePrevVal:
+                #if current cell value is None
+                if cell.value is None:
+                    print 'Col: ' + str(cell.col_idx)
+                    print 'Val+: ' + str(curr_row[cell.col_idx-1].value)
+                    #if saved prev row cell value is non None
+                    if curr_row[cell.col_idx-1].value is not None:
+                        cell.value = curr_row[cell.col_idx-1].value
+                else:
+                    print 'Col: ' + str(cell.col_idx)
+                    print 'Val: ' + str(cell.value)
+    else:
+        isFirstRow = False
 #save workbook
 print myhelpers.getNewFilePathWithDateNoSpaces(wbPath)
 wb.save(myhelpers.getNewFilePathWithDateNoSpaces(wbPath))
